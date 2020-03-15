7578526ffea5420813c4227827baece76b685265
yechan96 <yechanlee96@gmail.com>
2020-03-14 21:44:48 -0700

    done

--- app.js
+++ app.js
@@ -1,35 +1,139 @@
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+const OUTPUT_DIR = path.resolve("./", "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+const employees = [];
+
+
+const managerQuestions = [
+    {
+        type:'input',
+        name:'managerName',
+        message: 'Enter the manager\'s name:'
+
+    },
+    {
+        type:'input',
+        name:'managerID',
+        message: 'Enter the manager\'s ID number:'
+
+    },
+    {
+        type:'input',
+        name:'managerEmail',
+        message: 'Enter the manager\'s email:'
+
+    },
+    {
+        type:'input',
+        name:'managerOffice',
+        message: 'Enter the manager\'s office number:'
+
+    }
+];
+
+const engineerQuestions = [
+    {
+        type:'input',
+        name:'engineerName',
+        message: 'Enter the engineer\'s name:'
+
+    },
+    {
+        type:'input',
+        name:'engineerID',
+        message: 'Enter the engineer\'s ID number:'
+
+    },
+    {
+        type:'input',
+        name:'engineerEmail',
+        message: 'Enter the engineer\'s email:'
+
+    },
+    {
+        type:'input',
+        name:'engineerGithub',
+        message: 'Enter the engineer\'s Github username:'
+
+    }
+];
+
+const internQuestions = [
+    {
+        type:'input',
+        name:'internName',
+        message: 'Enter the intern\'s name:'
+
+    },
+    {
+        type:'input',
+        name:'internID',
+        message: 'Enter the intern\'s ID number:'
+
+    },
+    {
+        type:'input',
+        name:'internEmail',
+        message: 'Enter the intern\'s email:'
+
+    },
+    {
+        type:'input',
+        name:'internSchool',
+        message: 'Entern the intern\'s school:'
+
+    }
+];
+
+async function main(){
+    const manager = await inquirer.prompt(managerQuestions);
+    employees.push(new Manager(manager.managerName,manager.managerID,manager.managerEmail,manager.managerOffice));
+    let moreEmployee = true;
+    while(moreEmployee){
+        let isMore = await inquirer.prompt({
+            type:'list',
+            name:'choice',
+            message: 'Do you have more team members?',
+            choices: ["Yes","No"]
+        });
+        if(isMore.choice =="No"){
+            moreEmployee=false;
+        }
+        else{
+            let typeOfEmployee = await inquirer.prompt({
+                type:'list',
+                name:'choice',
+                message: 'What is their role?',
+                choices: ["Engineer","Intern"]
+            });
+            if(typeOfEmployee.choice == "Engineer"){
+                let engineer = await inquirer.prompt(engineerQuestions);
+                employees.push(new Engineer(engineer.engineerName,engineer.engineerID,engineer.engineerEmail,engineer.engineerGithub));
+
+            }
+            else{
+                let intern = await inquirer.prompt(internQuestions);
+                employees.push(new Intern(intern.internName,intern.internID,intern.internEmail,intern.internSchool));
+            }
+        }
+    }
+    
+    fs.mkdir(OUTPUT_DIR,{recursive:true},(err)=>{
+        if(err) throw err;
+    });
+    fs.writeFileSync(outputPath,render(employees));
+    
+};
+
+main();
+
